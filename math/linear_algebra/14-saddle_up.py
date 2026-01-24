b8ef0c27c32030fc035263f45a3e9b195dcb170d
hasby-umutoniwabo <h.umutoniwa@alustudent.com>
2026-01-24 08:05:51 -0800

    14

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module for matrix multiplication with numpy"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """Performs matrix multiplication"""
+    return np.matmul(mat1, mat2)
